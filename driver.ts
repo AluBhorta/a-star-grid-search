3822d6915bb3d4a07c01c4e9e32a4b6d230dc5a0
AluBhorta <dev.bhorta@gmail.com>
2019-10-09 23:12:39 +0600

    update driver

--- driver.ts
+++ driver.ts
@@ -1,19 +1,33 @@
 import { GridCell } from "./models";
 import { AStarGridSearch } from "./AStarSearch";
 
-const row = 5,
-  col = 5,
-  src: GridCell = { colNumber: 0, rowNumber: 0 },
-  dest: GridCell = { colNumber: 2, rowNumber: 2 },
-  blocked: GridCell[] = [
+const totalRows = 5,
+  totalCols = 6,
+  source: GridCell = { rowNumber: 2, colNumber: 0 },
+  destination: GridCell = { rowNumber: 2, colNumber: 3 },
+  blockedCells: GridCell[] = [
     { rowNumber: 1, colNumber: 1 },
-    { rowNumber: 2, colNumber: 1 },
-    { rowNumber: 3, colNumber: 1 },
-
     { rowNumber: 1, colNumber: 2 },
-    { rowNumber: 1, colNumber: 3 },
-    { rowNumber: 2, colNumber: 3 },
-  ];
+    { rowNumber: 2, colNumber: 2 },
+    { rowNumber: 2, colNumber: 4 },
+    { rowNumber: 3, colNumber: 1 },
+    { rowNumber: 2, colNumber: 4 }
+  ],
+  rowStepCost = 2,
+  colStepCost = 1;
 
-const path = AStarGridSearch(row, col, src, dest, blocked);
-console.log(path);
+const path = AStarGridSearch(
+  totalRows,
+  totalCols,
+  source,
+  destination,
+  blockedCells,
+  rowStepCost,
+  colStepCost
+);
+
+if (path === null) {
+  console.log(`No path from source to destination exists, unfortunately. :( `);
+} else {
+  console.log(`The Path from source to destination is:\n`, path);
+}
